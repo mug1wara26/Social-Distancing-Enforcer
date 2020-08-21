94390fcf968ef9febaac662a2425605894a67fe3
mug1wara26 <aloysiusgsq@gmail.com>
2020-08-21 19:06:29 +0800

    Created display_frame function

--- pyCv2/HumanTracking.py
+++ pyCv2/HumanTracking.py
@@ -1,14 +1,5 @@
-import cv2
-cap = cv2.VideoCapture(0)
-
-while True:
+def display_frame(cap):
     ret, frame = cap.read()
+    if ret:
+        return frame
 
-    cv2.imshow('Camera', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-
-cv2.destroyAllWindows()

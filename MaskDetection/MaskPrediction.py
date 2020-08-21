4b972a390e46d0f0b2286964c971cf51cca4a30a
mug1wara26 <aloysiusgsq@gmail.com>
2020-08-22 06:14:07 +0800

    Recoded Human Detection

---
+++ MaskDetection/MaskPrediction.py
@@ -0,0 +1,48 @@
+import cv2
+import os
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import numpy as np
+
+
+def predict_values(frame, faceCascade, model):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    faces_list=[]
+    preds=[]
+    for (x, y, w, h) in faces:
+        face_frame = frame[y:y+h,x:x+w]
+        face_frame = cv2.cvtColor(face_frame, cv2.COLOR_BGR2RGB)
+        face_frame = cv2.resize(face_frame, (224, 224))
+        face_frame = img_to_array(face_frame)
+        face_frame = np.expand_dims(face_frame, axis=0)
+        face_frame =  preprocess_input(face_frame)
+        faces_list.append(face_frame)
+        if len(faces_list)>0:
+            preds = model.predict(faces_list)
+
+    return preds
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+
+    mask_model = load_model("../Model/mask_recog_ver2 (1).h5")
+    while True:
+        ret, frame = cap.read()
+
+        cv2.imshow('frame', frame)
+
+        print(predict_values(frame, face_cascade, mask_model))
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
--- pyCv2/HumanTracking.py
+++ pyCv2/HumanTracking.py
@@ -1,41 +1,51 @@
-from __future__ import print_function
-from imutils.object_detection import non_max_suppression
 import numpy as np
 import imutils
 import cv2
-from time import sleep
 
 
-def get_points(frame, threshold):
-    hog = cv2.HOGDescriptor()
-    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+def display_bounding_box(frame):
+    net = cv2.dnn.readNetFromCaffe("../Model/MobileNetSSD_deploy.prototxt.txt", "../Model/MobileNetSSD_deploy.caffemodel")
 
-    image = imutils.resize(frame, width=min(400, frame.shape[1]))
-    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-                                            padding=(8, 8), scale=1.05)
+    frame = imutils.resize(frame, width=400)
 
-    # apply non-maxima suppression to the bounding boxes using a
-    # fairly large overlap threshold to try to maintain overlapping
-    # boxes that are still people
-    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=threshold)
-    return pick
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                                 0.007843, (300, 300), 127.5)
 
+    net.setInput(blob)
+    detections = net.forward()
 
-def display_frame(cap, threshold):
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    for (xA, yA, wA, hA) in get_points(frame, threshold):
-        cv2.rectangle(frame, (wA + wA - xA, yA), (wA, hA), (0, 255, 0), 2)
-    if ret:
-        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > 0.2:
+            # extract the index of the class label from the
+            # `detections`, then compute the (x, y)-coordinates of
+            # the bounding box for the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            # draw the prediction on the frame
+            label = "{}: {:.2f}%".format("Person",
+                                         confidence * 100)
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                          (0, 255, 0), 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(frame, label, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
 
     while True:
-        cv2.imshow('frame', cv2.cvtColor(display_frame(cap), cv2.COLOR_RGB2BGR))
+        ret, frame = cap.read()
+
+        display_bounding_box(frame)
+
+        cv2.imshow("Frame", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break

f55f77744e1a27249e368a80c1fdee48e5dc0ce8
notmeegoreng <66529710+notmeegoreng@users.noreply.github.com>
2020-08-21 20:17:58 +0800

    Make thing work
    main.py actually works now

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import wx
+import cv2
+import functools
 import UI.image
 from pyCv2 import HumanTracking
 
 app = wx.App()
 main_frame = wx.Frame(None)
-img_panel = UI.image.CV2ImagePanel(main_frame, HumanTracking.display_frame)  # lambda is placeholder for the img factory
+img_panel = UI.image.CV2ImagePanel(main_frame, functools.partial(HumanTracking.display_frame, cv2.VideoCapture(0)))
 main_frame.Show()
 app.MainLoop()
 

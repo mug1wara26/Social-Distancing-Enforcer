5e8138736e29dceeec8265126d7998c7634a4f6b
notmeegoreng <66529710+notmeegoreng@users.noreply.github.com>
2020-08-21 20:14:09 +0800

    moving stuff
    Stuff goes in main.py instead of mainui to fix stuffs

--- UI/mainui.py
+++
@@ -1,9 +0,0 @@
-import wx
-from .image import CV2ImagePanel
-
-def main():
-    app = wx.App()
-    main_frame = wx.Frame(None)
-    cap = CV2ImagePanel(main_frame, lambda: 1)  # lambda is placeholder for the img factory
-    main_frame.Show()
-    app.MainLoop()
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import UI.mainui
+import wx
+import UI.image
+from pyCv2 import HumanTracking
 
-UI.mainui.main()
+app = wx.App()
+main_frame = wx.Frame(None)
+img_panel = UI.image.CV2ImagePanel(main_frame, HumanTracking.display_frame)  # lambda is placeholder for the img factory
+main_frame.Show()
+app.MainLoop()
+

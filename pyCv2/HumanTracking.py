3a48751454d6dacd228b69f3f67d79c6cfb4287e
mug1wara26 <aloysiusgsq@gmail.com>
2020-08-21 19:27:32 +0800

    changed display_frame to return rgb

--- pyCv2/HumanTracking.py
+++ pyCv2/HumanTracking.py
@@ -1,5 +1,11 @@
+import cv2
+
+#def get_points(frame):
+#
+#
+
+
 def display_frame(cap):
     ret, frame = cap.read()
     if ret:
-        return frame
-
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)

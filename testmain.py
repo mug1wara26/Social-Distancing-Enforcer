c23878eab4985e6120c7140fe7bbdd34582c707e
notmeegoreng <66529710+notmeegoreng@users.noreply.github.com>
2020-08-21 22:25:15 +0800

    Clicky
    can detect mouse clicks now

--- testmain.py
+++ testmain.py
@@ -2,4 +2,8 @@ import wx
 import UI.image
 
 app = wx.App()
-
+main_frame = wx.Frame(None)
+click_frame = UI.image.PointSelectionPanel(main_frame)
+main_frame.Show()
+print(dir(wx.MouseEvent))
+app.MainLoop()

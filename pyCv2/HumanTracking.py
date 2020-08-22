ca564c13f0925668a013ede72a99703f49238aa3
mug1wara26 <aloysiusgsq@gmail.com>
2020-08-22 10:59:06 +0800

    scaled points semi correctly

--- pyCv2/HumanTracking.py
+++ pyCv2/HumanTracking.py
@@ -2,12 +2,12 @@ import numpy as np
 import imutils
 import cv2
 
-def get_boundaries(oriframe, threshold):
+def get_boundaries(cap, threshold):
     net = cv2.dnn.readNetFromCaffe("../Model/MobileNetSSD_deploy.prototxt.txt",
                                    "../Model/MobileNetSSD_deploy.caffemodel")
 
-    #ret, innerframe = cap.read()
-    innerframe = imutils.resize(oriframe, width=400)
+    ret, innerframe = cap.read()
+    innerframe = imutils.resize(innerframe, width=400)
 
     (h, w) = innerframe.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(innerframe, (300, 300)),
@@ -45,9 +45,6 @@ def display_frame(frame, innerframe, dimensions):
         endY = int(dimension[3] * y)
         confidence = dimension[4]
 
-        print(dimension)
-        print(startX, startY, endX, endY, frame.shape)
-
         cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
         y = startY - 15 if startY - 15 > 15 else startY + 15
         # draw the prediction on the frame
@@ -55,6 +52,31 @@ def display_frame(frame, innerframe, dimensions):
                                      confidence * 100)
         cv2.putText(frame, label, (startX, y),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        #Draw circles on frame correctly
+        hasDanger = 0
+        trc = bottomCentres(dimensions)
+        points = np.array([[53, 248], [87, 198], [141, 205], [117, 257]])  # pass 4 points here
+        height, width, hMatrix = transformInfo(points, 5, 5, 500)  # pass width, height where 5, 5 is
+        newTrc = transformPoints(trc, hMatrix)
+        warped = transformedImage(frame, newTrc, 1500, 1500, hMatrix, height, width, trc)
+        clusterIndex = [0] * len(trc)
+        dbscan(3, 1, newTrc, height, width, clusterIndex)
+        # cv2.imshow("Warped", warped)
+        vals = [(0, 0, 255), (0, 0, 0), (255, 0, 0), (255, 255, 0), (0, 255, 255), (255, 255, 255)]
+        toIncrement = 0
+        for i in range(len(clusterIndex)):
+            if (clusterIndex.count(clusterIndex[i]) >= 2 and clusterIndex[i] != -1):
+                toIncrement += 1
+            cv2.circle(frame, (int(trc[i][0] * x), int(trc[i][1] * y)), radius=3, color=vals[clusterIndex[i]], thickness=-1)
+        if (toIncrement != 0):
+            hasDanger += toIncrement
+        else:
+            hasDanger = 0
+            print("no more AAAA")
+        cv2.imshow("Frame", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        if (hasDanger > 6):
+            print("AAAA")
 
     return cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
@@ -153,34 +175,12 @@ def get_ratio(orimage, transimage):
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("../resources/View_001/frame_%04d.jpg", cv2.CAP_IMAGES)
-    hasDanger = 0
     while True:
         transimage, dimensions = get_boundaries(cap, 0.1)
         ret, orimage = cap.read()
-        trc = bottomCentres(dimensions)
 
         frame = display_frame(orimage, transimage, dimensions)
-        points = np.array([[53, 248], [87, 198], [141, 205], [117, 257]]) #pass 4 points here
-        height, width, hMatrix = transformInfo(points, 5, 5, 500) #pass width, height where 5, 5 is
-        newTrc = transformPoints(trc, hMatrix)
-        warped = transformedImage(frame, newTrc, 1500, 1500, hMatrix, height, width, trc)
-        clusterIndex = [0] * len(trc)
-        dbscan(3, 1, newTrc, height, width, clusterIndex)
-        #cv2.imshow("Warped", warped)
-        vals = [(0, 0, 255), (0, 0, 0), (255, 0, 0), (255, 255, 0), (0, 255, 255), (255, 255, 255)]
-        toIncrement = 0
-        for i in range(len(clusterIndex)):
-            if(clusterIndex.count(clusterIndex[i]) >= 2 and clusterIndex[i] != -1):
-                toIncrement += 1
-            cv2.circle(frame, (trc[i][0], trc[i][1]), radius=3, color=vals[clusterIndex[i]], thickness=-1)
-        if(toIncrement != 0):
-            hasDanger += toIncrement
-        else:
-            hasDanger = 0
-            print("no more AAAA")
-        cv2.imshow("Frame", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        if(hasDanger > 6):
-            print("AAAA")
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
